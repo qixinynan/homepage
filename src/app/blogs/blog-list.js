07b701dff26ebbcfbfb71ac731b9e2ad73693156
Qixiny <3185835784@qq.com>
2024-08-22 00:44:09 +0800

    fix: style bugs

--- src/app/blogs/blog-list.js
+++ src/app/blogs/blog-list.js
@@ -3,8 +3,8 @@ import Card from "../components/common/card";
 import H2 from "../components/common/h2";
 export default async function BlogList({}) {
   const blogs = (await getBlogs()).data;
-  return <div className="my-5 mb-10">
-    <H2>博客们：</H2>
+  return <div className="my-5 mb-10 ">
+    <H2 className="ml-2">博客们：</H2>
     {blogs.map((blog, i) => (
       <a href={`/blogs/${blog.id}`}>
         <Card key={i} className="p-3 mt-3">
